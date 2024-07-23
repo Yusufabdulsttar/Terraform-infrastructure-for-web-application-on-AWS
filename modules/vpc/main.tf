968ba9aa570ae0259190b2a66ae84bd467751b7e
Yusuf Abdulsttar <134774623+Yusufabdulsttar@users.noreply.github.com>
2024-07-23 21:44:10 +0300

    Add VPC Variables

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -1,19 +1,19 @@
 // Create a VPC with the specified CIDR block
 resource "aws_vpc" "vpc" {
-  cidr_block = "10.0.0.0/16"
+  cidr_block = var.cidr_block_vpc
 }
 
 // Create a public subnet within the VPC
 resource "aws_subnet" "public_subnet" {
   vpc_id            = aws_vpc.vpc.id
-  cidr_block        = "10.0.1.0/24"
+  cidr_block        = var.cidr_block_public_subnet
   map_public_ip_on_launch = true
 }
 
 // Create a private subnet within the VPC
 resource "aws_subnet" "private_subnet" {
   vpc_id     = aws_vpc.vpc.id
-  cidr_block = "10.0.2.0/24"
+  cidr_block = var.cidr_block_private_subnet
 }
 
 // Create an Internet Gateway for the VPC
@@ -26,13 +26,13 @@ resource "aws_route_table" "public" {
   vpc_id = aws_vpc.vpc.id
 
   route {
-    cidr_block = "0.0.0.0/0"
+    cidr_block = var.cidr_block_route_table
     gateway_id = aws_internet_gateway.igw.id
   }
 }
 
-// Associate the public route table with the public subnet
-resource "aws_route_table_association" "public" {
+// Associate the public route table with the private subnet
+resource "aws_route_table_association" "private" {
   subnet_id      = aws_subnet.public_subnet.id
   route_table_id = aws_route_table.public.id
 }
